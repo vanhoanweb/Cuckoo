9bd0b89db11831d046dfae0846c05d6cfb191b09
Van Hoan <vanhoanweb@gmail.com>
2016-10-08 14:26:58 +0700

    Update search.php

--- search.php
+++ search.php
@@ -3,22 +3,22 @@
 <section id="content" role="main">
 	<?php if ( have_posts() ) : ?>
 		<header class="header">
-			<h1 class="entry-title"><?php printf( __( 'Search Results for: %s', 'zeroblank' ), get_search_query() ); ?></h1>
+			<h1 class="entry-title"><?php printf( __( 'Search Results for: %s', 'zb' ), get_search_query() ); ?></h1>
 		</header>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'entry' ); ?>
 		<?php endwhile; ?>
 
-		<?php zeroblank_pagenavi(); ?>
+		<?php get_template_part('pagination'); ?>
 	<?php else : ?>
 		<article id="post-0" class="post no-results not-found">
 			<header class="header">
-				<h2 class="entry-title"><?php _e( 'Nothing Found', 'zeroblank' ); ?></h2>
+				<h2 class="entry-title"><?php _e( 'Nothing Found', 'zb' ); ?></h2>
 			</header>
 
 			<section class="entry-content">
-				<p><?php _e( 'Sorry, nothing matched your search. Please try again.', 'zeroblank' ); ?></p>
+				<p><?php _e( 'Sorry, nothing matched your search. Please try again.', 'zb' ); ?></p>
 				<?php get_search_form(); ?>
 			</section>
 		</article>
