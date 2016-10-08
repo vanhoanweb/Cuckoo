5da20fc3a5c8b664b657e62d309a01bb9012f3db
Van Hoan <vanhoanweb@gmail.com>
2016-10-08 14:23:26 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
 		<footer id="footer" role="contentinfo">
 			<div id="copyright">
-			<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'zeroblank' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?>
+			<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'zb' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?>
 			</div>
 		</footer>
 	</div><!-- #wrapper -->
