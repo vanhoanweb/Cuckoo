6bec779454a09e1b07147ab5a54f7b02533c0589
Van Hoan <vanhoanweb@gmail.com>
2016-10-08 14:38:39 +0700

    Update single.php

--- single.php
+++ single.php
@@ -7,10 +7,7 @@
 	<?php endwhile; endif; ?>
 
 	<footer class="footer">
-		<nav id="nav-below" class="navigation" role="navigation">
-			<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">&larr;</span> %title' ); ?></div>
-			<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">&rarr;</span>' ); ?></div>
-		</nav>
+		<?php get_template_part('pagination'); ?>
 	</footer>
 </section>
 
