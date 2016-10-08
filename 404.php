d079a02f82bf481e4e7973d5f0ca494a1ab7d79e
Van Hoan <vanhoanweb@gmail.com>
2016-10-08 14:13:56 +0700

    Update 404.php

--- 404.php
+++ 404.php
@@ -3,11 +3,11 @@
 <section id="content" role="main">
 	<article id="post-0" class="post not-found">
 		<header class="header">
-			<h1 class="entry-title"><?php _e( 'Not Found', 'zeroblank' ); ?></h1>
+			<h1 class="entry-title"><?php _e( 'Not Found', 'zb' ); ?></h1>
 		</header>
 
 		<section class="entry-content">
-			<p><?php _e( 'Nothing found for the requested page. Try a search instead?', 'zeroblank' ); ?></p>
+			<p><?php _e( 'Nothing found for the requested page. Try a search instead', 'zb' ); ?></p>
 			<?php get_search_form(); ?>
 		</section>
 	</article>
