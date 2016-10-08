e1ebaadaff7eb8e97fde5669a1dd3a9d78f22af4
Van Hoan <vanhoanweb@gmail.com>
2016-10-08 14:39:30 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
-add_action( 'after_setup_theme', 'zeroblank_setup' );
-function zeroblank_setup() {
-	// load_theme_textdomain( 'zeroblank', get_template_directory() . '/languages' );
+add_action( 'after_setup_theme', 'zb_setup' );
+function zb_setup() {
+	// load_theme_textdomain( 'zb', get_template_directory() . '/languages' );
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'automatic-feed-links');
 	add_theme_support( 'menus' );
@@ -9,7 +9,7 @@ function zeroblank_setup() {
 
 	// Thumbnail size
 	/*$thumb_realisation_ratio = 1.5; // Using aspect ratio for WP 4.4 responsive image
-	add_image_size( 'zeroblank-custom', 700, round(700/$thumb_realisation_ratio), true );*/
+	add_image_size( 'zb-custom', 700, round(700/$thumb_realisation_ratio), true );*/
 
 	// Enable support for HTML5 markup.
 	add_theme_support( 'html5', array(
@@ -21,13 +21,13 @@ function zeroblank_setup() {
 	global $content_width;
 	if ( ! isset( $content_width ) ) $content_width = 640;
 	register_nav_menus(
-		array( 'main-menu' => __( 'Main Menu', 'zeroblank' ) )
+		array( 'main-menu' => __( 'Main Menu', 'zb' ) )
 	);
 }
 
 // Enqueue base scripts and styles
-add_action( 'wp_enqueue_scripts', 'zeroblank_scripts_and_styles' );
-function zeroblank_scripts_and_styles() {
+add_action( 'wp_enqueue_scripts', 'zb_scripts_and_styles' );
+function zb_scripts_and_styles() {
 	if ( is_admin() ) return;
 	wp_enqueue_style( 'stylesheet', get_stylesheet_uri() );
 	wp_enqueue_script( 'jquery' );
@@ -39,8 +39,8 @@ function zeroblank_scripts_and_styles() {
 	}
 }
 
-add_filter( 'the_title', 'zeroblank_title' );
-function zeroblank_title( $title ) {
+add_filter( 'the_title', 'zb_title' );
+function zb_title( $title ) {
 	if ( $title == '' ) {
 		return '&rarr;';
 	} else {
@@ -48,16 +48,16 @@ function zeroblank_title( $title ) {
 	}
 }
 
-add_filter( 'wp_title', 'zeroblank_filter_wp_title' );
-function zeroblank_filter_wp_title( $title ) {
+add_filter( 'wp_title', 'zb_filter_wp_title' );
+function zb_filter_wp_title( $title ) {
 	return $title . esc_attr( get_bloginfo( 'name' ) );
 }
 
 // Adding sidebars to Wordpress
-add_action( 'widgets_init', 'zeroblank_widgets_init' );
-function zeroblank_widgets_init() {
+add_action( 'widgets_init', 'zb_widgets_init' );
+function zb_widgets_init() {
 	register_sidebar( array (
-		'name' => __( 'Sidebar Widget Area', 'zeroblank' ),
+		'name' => __( 'Sidebar Widget Area', 'zb' ),
 		'id' => 'primary-widget-area',
 		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
 		'after_widget' => "</li>",
@@ -66,13 +66,13 @@ function zeroblank_widgets_init() {
 	) );
 }
 
-function zeroblank_custom_pings( $comment ) {
+function zb_custom_pings( $comment ) {
 	$GLOBALS['comment'] = $comment; ?>
 	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>"><?php echo comment_author_link(); ?></li>
 <?php }
 
-add_filter( 'get_comments_number', 'zeroblank_comments_number' );
-function zeroblank_comments_number( $count ) {
+add_filter( 'get_comments_number', 'zb_comments_number' );
+function zb_comments_number( $count ) {
 	if ( !is_admin() ) {
 		global $id;
 		$comments_by_type = &separate_comments( get_comments( 'status=approve&post_id=' . $id ) );
@@ -83,12 +83,12 @@ function zeroblank_comments_number( $count ) {
 }
 
 // Pagination for paged posts, Page 1, Page 2, Page 3, with Next and Previous Links, No plugin
-function zeroblank_pagenavi() {
+function zb_pagenavi() {
 	global $wp_query;
 	$bignum = 999999999;
 	if ( $wp_query->max_num_pages <= 1 )
 		return;
-	echo '<nav id="nav-below" class="navigation" role="navigation">';
+	echo '<nav class="pagination" role="navigation">';
 	echo paginate_links( array(
 		'base'			=> str_replace( $bignum, '%#%', esc_url( get_pagenum_link($bignum) ) ),
 		'format'		=> '',
@@ -104,8 +104,8 @@ function zeroblank_pagenavi() {
 }
 
 // Add 'Read more' button instead of [...] for Excerpts
-add_filter( 'excerpt_more', 'zeroblank_excerpt_more' );
-function zeroblank_excerpt_more( $more ) {
+add_filter( 'excerpt_more', 'zb_excerpt_more' );
+function zb_excerpt_more( $more ) {
 	global $post;
-	return '... <a class="read-more" href="' . get_permalink($post->ID) . '">' . __( 'Read more', 'zeroblank' ) . '</a>';
+	return '... <a class="read-more" href="' . get_permalink($post->ID) . '">' . __( 'Read more', 'zb' ) . '</a>';
 }
