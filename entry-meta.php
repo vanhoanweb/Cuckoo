adf3ad19e044dd5d2ba5710fd6231ae5d378987b
Van Hoan <vanhoanweb@gmail.com>
2016-10-08 14:21:30 +0700

    Update entry-meta.php

--- entry-meta.php
+++ entry-meta.php
@@ -3,7 +3,7 @@
 	<span class="entry-date"><?php the_time( get_option( 'date_format' ) ); ?></span>
 	<?php if ( comments_open( get_the_ID() ) ) :
 	echo '<span class="comments-link">';
-	comments_popup_link( __( 'Leave your thoughts', 'zeroblank' ), __( '1 Comment', 'zeroblank' ), __( '% Comments', 'zeroblank' ));
+	comments_popup_link( __( 'Leave your thoughts', 'zb' ), __( '1 Comment', 'zb' ), __( '% Comments', 'zb' ));
 	echo '</span>';
 	endif; ?>
 </section>
