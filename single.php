89f990d9e53582cb068ae593d10bd094c36d8d68
Van Hoan <vanhoanweb@gmail.com>
2016-10-09 11:26:16 +0700

    Update single.php

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<section id="content" role="main">
+<main id="content" role="main">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<?php get_template_part( 'entry' ); ?>
 	<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
@@ -9,7 +9,7 @@
 	<footer class="footer">
 		<?php get_template_part('pagination'); ?>
 	</footer>
-</section>
+</main>
 
 <?php get_sidebar(); ?>
 
