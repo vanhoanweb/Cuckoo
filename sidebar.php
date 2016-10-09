05a6314c27cdc9d86772b75a4baee879e17172b9
Van Hoan <vanhoanweb@gmail.com>
2016-10-09 11:25:53 +0700

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,5 @@
 <aside id="sidebar" role="complementary">
-	<?php if ( is_active_sidebar( 'primary-widget-area' ) ) : ?>
-		<div id="primary" class="widget-area">
-			<ul><?php dynamic_sidebar( 'primary-widget-area' ); ?></ul>
-		</div>
-	<?php endif; ?>
+	<?php if ( is_active_sidebar( 'primary-widget-area' ) ) {
+		dynamic_sidebar( 'primary-widget-area' );
+	} ?>
 </aside>
