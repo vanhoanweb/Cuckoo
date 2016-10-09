35b0b9845889b8776b056dc3181dc9908b3b6ae2
Van Hoan <vanhoanweb@gmail.com>
2016-10-09 11:24:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
-<section id="content" role="main">
+<main id="content" role="main">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<?php get_template_part( 'entry' ); ?>
 		<?php comments_template(); ?>
 	<?php endwhile; endif; ?>
 
 	<?php get_template_part('pagination'); ?>
-</section>
+</main>
 
 <?php get_sidebar(); ?>
 
