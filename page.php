59fb53577d75eb556510b39541231f59df8c865e
Van Hoan <vanhoanweb@gmail.com>
2016-10-09 11:24:57 +0700

    Update page.php

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<section id="content" role="main">
+<main id="content" role="main">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<header class="header">
@@ -16,7 +16,7 @@
 
 		<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
 	<?php endwhile; endif; ?>
-</section>
+</main>
 
 <?php get_sidebar(); ?>
 
