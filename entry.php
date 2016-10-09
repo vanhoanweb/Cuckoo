28ba6f4e1f77688db544d65ff2bdc8df27c43520
Van Hoan <vanhoanweb@gmail.com>
2016-10-09 11:22:09 +0700

    Update entry.php

--- entry.php
+++ entry.php
@@ -1,9 +1,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header>
-		<?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
+		<?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title"><a href="'. get_permalink() .'" title="'. get_the_title() .'" rel="bookmark">'; } ?><?php the_title(); ?><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</a></h2>'; } ?>
 		<?php if ( !is_search() ) get_template_part( 'entry', 'meta' ); ?>
 	</header>
 
-	<?php get_template_part( 'entry', ( is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
+	<?php get_template_part( 'entry', ( is_singular() ? 'content' : 'summary' ) ); ?>
 	<?php if ( !is_search() ) get_template_part( 'entry-footer' ); ?>
 </article>
