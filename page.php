2d89a7b9216171e8553831f6d87d89dbc3c7af45
Van Hoan <vanhoanweb@gmail.com>
2016-10-17 11:49:02 +0700

    Update page.php

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 <main id="content" role="main">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<header class="header">
+			<header class="entry-header">
 				<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
 			</header>
 
