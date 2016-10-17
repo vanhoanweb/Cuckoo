8e7c1ca6ed65fae3463fa09ce02d7ece10686df6
Van Hoan <vanhoanweb@gmail.com>
2016-10-17 11:50:26 +0700

    Update entry-meta.php

--- entry-meta.php
+++ entry-meta.php
@@ -1,8 +1,8 @@
 <section class="entry-meta">
-	<span class="author vcard"><?php the_author_posts_link(); ?></span>
-	<span class="entry-date"><?php the_time( get_option( 'date_format' ) ); ?></span>
+	<span class="entry-time"><?php the_time( get_option( 'date_format' ) ); ?></span>
+	<span class="entry-author"><?php the_author_posts_link(); ?></span>	
 	<?php if ( comments_open( get_the_ID() ) ) :
-	echo '<span class="comments-link">';
+	echo '<span class="entry-comments-link">';
 	comments_popup_link( __( 'Leave your thoughts', 'zb' ), __( '1 Comment', 'zb' ), __( '% Comments', 'zb' ));
 	echo '</span>';
 	endif; ?>
