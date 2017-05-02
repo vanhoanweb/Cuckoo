7d74fb5a6491f74ca5ad4cf9b83f2edc4d0b43fe
Van Hoan <vanhoanweb@gmail.com>
2017-05-02 22:54:32 +0700

    Create not-found.php

---
+++ inc/not-found.php
@@ -0,0 +1,3 @@
+<div class="entry">
+	<p><?php _e('Sorry, but the requested resource was not found on this site. Please try again or contact the administrator for assistance.', 'zero-blank'); ?></p>
+</div>
