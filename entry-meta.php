e2ddd9cbd72ec40a5b8315e741167da3a4ed14a9
Van Hoan <vanhoanweb@gmail.com>
2017-05-02 22:39:10 +0700

    Delete entry-meta.php

--- entry-meta.php
+++
@@ -1,9 +0,0 @@
-<section class="entry-meta">
-	<span class="entry-time"><?php the_time( get_option( 'date_format' ) ); ?></span>
-	<span class="entry-author"><?php the_author_posts_link(); ?></span>	
-	<?php if ( comments_open( get_the_ID() ) ) :
-	echo '<span class="entry-comments-link">';
-	comments_popup_link( __( 'Leave your thoughts', 'zb' ), __( '1 Comment', 'zb' ), __( '% Comments', 'zb' ));
-	echo '</span>';
-	endif; ?>
-</section>
