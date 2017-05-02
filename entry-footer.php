17c37b600d74992cc656ef7bba40c832701d8246
Van Hoan <vanhoanweb@gmail.com>
2017-05-02 22:38:54 +0700

    Delete entry-footer.php

--- entry-footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="entry-footer">
-	<span class="cat-links"><?php _e( 'Categories: ', 'zb' ); ?><?php the_category( ', ' ); ?></span>
-	<span class="tag-links"><?php the_tags(); ?></span>
-	<?php edit_post_link(); ?>
-</footer> 
