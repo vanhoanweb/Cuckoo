cffc96b41d1e4220b82be90a02ffeefd4285c1f5
Van Hoan <vanhoanweb@gmail.com>
2017-05-02 22:39:40 +0700

    Delete entry.php

--- entry.php
+++
@@ -1,9 +0,0 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header>
-		<?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title"><a href="'. get_permalink() .'" title="'. get_the_title() .'" rel="bookmark">'; } ?><?php the_title(); ?><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</a></h2>'; } ?>
-		<?php if ( !is_search() ) get_template_part( 'entry', 'meta' ); ?>
-	</header>
-
-	<?php get_template_part( 'entry', ( is_singular() ? 'content' : 'summary' ) ); ?>
-	<?php if ( !is_search() ) get_template_part( 'entry-footer' ); ?>
-</article>
