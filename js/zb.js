bb5b45c2b0f216df6b05912bd0d0f188e8dd7b1c
Van Hoan <vanhoanweb@gmail.com>
2017-05-06 22:26:05 +0700

    Update zb.js

--- js/zb.js
+++ js/zb.js
@@ -1,5 +1,25 @@
 var $j = jQuery.noConflict();
 
 $j(function() {
+	
+	//responsive menu
+	$j(".nav-primary .zb-nav-menu, .nav-secondary .zb-nav-menu").addClass("responsive-menu").before('<div class="responsive-menu-icon"></div>');
 
+	$j(".responsive-menu-icon").click(function(){
+		$j(this).next(".nav-primary .zb-nav-menu, .nav-secondary .zb-nav-menu").slideToggle();
+	});
+
+	$j(window).resize(function(){
+		if(window.innerWidth > 767) {
+			$j(".nav-primary .zb-nav-menu, .nav-secondary .zb-nav-menu, nav .sub-menu").removeAttr("style");
+			$j(".responsive-menu > .menu-item").removeClass("menu-open");
+		}
+	});
+
+	$j(".responsive-menu > .menu-item").click(function(event){
+		if (event.target !== this) return;
+		$j(this).find(".sub-menu:first").slideToggle(function() {
+			$j(this).parent().toggleClass("menu-open");
+		});
+	});
 });
