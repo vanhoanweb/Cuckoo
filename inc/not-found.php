b7af0f5366f635a491c312e8aa772a5967c2c5b6
Van Hoan <vanhoanweb@gmail.com>
2017-05-06 22:25:25 +0700

    Update not-found.php

--- inc/not-found.php
+++ inc/not-found.php
@@ -1,3 +1,3 @@
-<div class="entry">
+<div class="entry">	
 	<p><?php _e('Sorry, but the requested resource was not found on this site. Please try again or contact the administrator for assistance.', 'zero-blank'); ?></p>
 </div>
