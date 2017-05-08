839a987ede5dd6bfdf36c2ce4edfa0e28fc577df
Van Hoan <vanhoanweb@gmail.com>
2017-05-08 21:30:10 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,26 +2,35 @@
 
 if (!defined('ABSPATH')) exit;
 
-function zb_setup() {	
+function zb_setup() {
+	// translation
+	// load_theme_textdomain('zero-blank', get_template_directory() .'/languages');
+
 	// define max content width
 	global $content_width;
 	if (!isset($content_width)) $content_width = 960;
 
 	// set up theme support
-	add_theme_support('post-thumbnails');
 	add_theme_support('automatic-feed-links');
-	add_theme_support('custom-background');
+	add_theme_support('title-tag');
+	add_theme_support('post-thumbnails');
 	add_theme_support('custom-header');
 	add_theme_support('custom-logo');
+	add_theme_support('custom-background');
+	add_theme_support('html5', array(
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	));
+	
+	// menu
 	add_theme_support('menus');
-	add_theme_support('title-tag');
-
-	// register menu
 	register_nav_menus(array(
 		'primary-navigation'	=>	__('Primary Navigation', 'zero-blank'),
 		'secondary-navigation'	=>	__('Secondary Navigation', 'zero-blank')
-	));
-	
+	));	
 }
 add_action('after_setup_theme', 'zb_setup');
 
@@ -30,10 +39,10 @@ function zb_scripts_and_styles() {
 	if (is_admin()) return;
 	
 	// wp_enqueue_style($handle, $src, $deps, $ver, $media)
-	wp_enqueue_style('zb', get_stylesheet_uri(), array(), null);
+	wp_enqueue_style('zb-main', get_stylesheet_uri(), array(), null);
 	
 	// wp_enqueue_script($handle, $src, $deps, $ver, $in_footer)
-	wp_enqueue_script('zb', get_template_directory_uri() .'/js/zb.js', array('jquery'), null, true);
+	wp_enqueue_script('zb-main', get_template_directory_uri() .'/js/zb.js', array('jquery'), null, true);
 	
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
@@ -42,8 +51,8 @@ function zb_scripts_and_styles() {
 add_action('wp_enqueue_scripts', 'zb_scripts_and_styles');
 
 // register widgets
-function zb_widgets_init() {	
-	$widget_args_1 = array(		
+function zb_widgets_init() {
+	$widget_args_1 = array(
 		'name'			=>	__('Widgets Sidebar', 'zero-blank'),
 		'id'			=>	'widgets_sidebar',
 		'class'			=>	'',
@@ -51,9 +60,9 @@ function zb_widgets_init() {
 		'before_widget'	=>	'<section class="widget %s">',
 		'after_widget'	=>	'</section>',
 		'before_title'	=>	'<h4 class="widget-title widgettitle">',
-		'after_title'	=>	'</h4>'		
+		'after_title'	=>	'</h4>'
 	);	
-	register_sidebar($widget_args_1);	
+	register_sidebar($widget_args_1);
 }
 add_action('widgets_init', 'zb_widgets_init');
 
