36f92d6b0e0a6e8acbcb83e09354811e9707882b
Van Hoan <vanhoanweb@gmail.com>
2017-05-09 10:42:47 +0700

    Update content.php

--- inc/content.php
+++ inc/content.php
@@ -14,6 +14,7 @@
 				<h2 class="entry-title" itemprop="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<?php endif; ?>
 
+			<?php if (get_post()->post_type == 'post'): ?>
 			<p class="entry-meta">
 				<span class="entry-time">
 					<em><?php _e('Posted on ', 'zero-blank'); ?></em><time itemprop="datePublished" datetime="<?php the_time('Y-m-d'); ?>" pubdate><?php the_time('F jS, Y'); ?></time>
@@ -26,6 +27,7 @@
 				<?php endif; ?>
 				<?php edit_post_link('Edit this &raquo;', '<span class="entry-edit">', '</span>'); ?>
 			</p>
+			<?php endif; ?>
 		</header>
 
 		<div class="entry-content" itemprop="text">
@@ -37,12 +39,12 @@
 		</div>
 
 		<?php if (get_post()->post_type == 'post'): ?>
-			<footer class="entry-footer">
-				<p class="entry-meta">
-					<span class="entry-categories"><?php _e('Filed Under: ', 'zero-blank'); ?><?php the_category(', '); ?></span>
-					<span class="entry-tags"><?php the_tags('Tagged With: '); ?></span>
-				</p>
-			</footer>
+		<footer class="entry-footer">
+			<p class="entry-meta">
+				<span class="entry-categories"><?php _e('Filed Under: ', 'zero-blank'); ?><?php the_category(', '); ?></span>
+				<span class="entry-tags"><?php the_tags('Tagged With: '); ?></span>
+			</p>
+		</footer>
 		<?php endif; ?>
 
 	</article>
