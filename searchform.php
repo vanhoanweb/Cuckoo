a8db38ad45fbb1651126a6aaa18e77b5a99fd764
Van Hoan <vanhoanweb@gmail.com>
2017-05-09 10:41:23 +0700

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,5 +1,4 @@
-<form method="get" class="search-form" action="<?php echo home_url('/'); ?>">
-	<label for="s" class="search-form-label screen-reader-text"><?php _e('Search for: ', 'zero-blank'); ?></label>
+<form class="search-form" itemprop="potentialAction" itemscope="" itemtype="http://schema.org/SearchAction" method="get" action="<?php echo home_url('/'); ?>" role="search">
 	<input type="search" class="search-field" name="s" id="s" placeholder="Search...">
 	<input type="submit" class="search-submit" value="Search">
 </form>
