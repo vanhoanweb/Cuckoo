f6040ccbd5e12058a1bf66be5e4b9cf7faf5feb9
Van Hoan <vanhoanweb@gmail.com>
2017-05-09 12:49:32 +0700

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,58 +1,56 @@
-<?php get_header(); ?>
+<?php while (have_posts()) : the_post(); ?>
+	
+	<article <?php post_class('entry'); ?> itemscope="" itemtype="http://schema.org/CreativeWork">
 
-<main class="content">
+		<?php if (has_post_thumbnail()) : ?>
+		<a class="featured-image" href="<?php the_permalink(); ?>">
+			<?php the_post_thumbnail('large', array('class' => 'entry-image')); ?>
+		</a><?php endif; ?>
 
-	<article <?php post_class('entry'); ?>>
-		
-		<h1 class="entry-title"><?php _e('Not Found, error 404', 'zero-blank'); ?></h1>
+		<header class="entry-header">
+			<?php if (is_singular()): ?>
+				<h1 class="entry-title" itemprop="headline"><?php the_title(); ?></h1>
+			<?php else : ?>
+				<h2 class="entry-title" itemprop="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<?php endif; ?>
 
-		<div class="entry-content">
-			
-			<p>The page you are looking for no longer exists. Perhaps you can return back to the site's <a href="<?php echo home_url('/'); ?>">homepage</a> and see if you can find what you are looking for. Or, you can try finding it by using the search form below.</p>
+			<?php if (get_post()->post_type == 'post'): ?>
+			<p class="entry-meta">
+				<span class="entry-time">
+					<em><?php _e('Posted on ', 'zero-blank'); ?></em><time itemprop="datePublished" datetime="<?php the_time('Y-m-d'); ?>" pubdate><?php the_time('F jS, Y'); ?></time>
+				</span>
+				<span class="entry-author" itemprop="author" itemscope="" itemtype="http://schema.org/Person">
+					<em><?php _e('Written by ', 'zero-blank'); ?></em><?php the_author_posts_link(); ?>
+				</span>
+				<?php if (comments_open(get_the_ID())): ?>
+					<span class="entry-comments"><?php comments_popup_link('Leave a comment', '1 comment', '% comments', '', 'Comments disabled'); ?></span>
+				<?php endif; ?>
+				<?php edit_post_link('Edit this &raquo;', '<span class="entry-edit">', '</span>'); ?>
+			</p>
+			<?php endif; ?>
+		</header>
 
-			<?php get_search_form(); ?>
-
-			<h2>Sitemap</h2>
-
-			<h4><?php _e('Pages:', 'zero-blank'); ?></h4>
-				<ul><?php wp_list_pages('depth=&title_li='); ?></ul>
-
-				<h4><?php _e('Categories:', 'zero-blank'); ?></h4>
-				<ul><?php wp_list_categories('orderby=name&order=ASC&show_count=0&title_li=&depth=0'); ?></ul>
-
-				<h4><?php _e('Authors:', 'zero-blank'); ?></h4>
-				<ul><?php wp_list_authors(array(
-					'show_fullname'	=> 1,
-					'optioncount'	=> 1,
-					'orderby'		=> 'post_count',
-					'order'			=> 'DESC',
-					'number'		=> 10,
-					'hide_empty'	=> 0,
-					'echo'			=> 1,
-					'exclude_admin'	=> 0
-				)); ?></ul>
-
-				<h4><?php _e('Monthly:', 'zero-blank'); ?></h4>
-				<ul><?php wp_get_archives(array('type' => 'monthly')); ?></ul>
-
-				<h4><?php _e('Recent Posts:', 'zero-blank'); ?></h4>
-				<?php $args = array('posts_per_page' => 10);
-				$recent_posts = new WP_Query($args);
-				if($recent_posts->have_posts()):
-					echo '<ul>';
-					while ($recent_posts->have_posts()) : $recent_posts->the_post();
-						echo '<li><a href="'.get_the_permalink().'">'.get_the_title().'</a></li>';
-					endwhile;
-					echo '</ul>';
-				endif;
-				wp_reset_postdata(); ?>
-
+		<div class="entry-content" itemprop="text">
+			<?php if (is_singular()): ?>
+				<?php the_content(); ?>
+			<?php else : ?>
+				<?php the_excerpt(); ?>
+			<?php endif; ?>
 		</div>
-		
+
+		<?php if (get_post()->post_type == 'post'): ?>
+		<footer class="entry-footer">
+			<p class="entry-meta">
+				<span class="entry-categories"><?php _e('Filed Under: ', 'zero-blank'); ?><?php the_category(', '); ?></span>
+				<span class="entry-tags"><?php the_tags('Tagged With: '); ?></span>
+			</p>
+		</footer>
+		<?php endif; ?>
+
 	</article>
 
-</main>
+<?php endwhile; ?>
 
-<?php get_sidebar(); ?>
+<?php get_template_part('inc/pagination'); ?>
 
-<?php get_footer(); ?>
+<?php if (is_singular()) comments_template(); ?>
