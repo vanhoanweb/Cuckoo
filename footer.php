3ef5d2c2a01879ed0f7d56fb772ffd0f62624d6a
Van Hoan <vanhoanweb@gmail.com>
2017-05-27 11:48:51 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,13 +5,27 @@
 		<div class="footer-widgets">
 			<div class="wrap">
 				<div class="footer-widgets-1 widget-area">
-					<?php the_widget('WP_Widget_Categories', '', 'before_widget=<section class="widget %s"><div class="widget-wrap">&after_widget=</div></section>&before_title=<h4 class="widget-title widgettitle">&after_title=</h4>'); ?>
+					<?php if (is_active_sidebar('footer-sidebar-1')) :
+						dynamic_sidebar('footer-sidebar-1');
+					else :
+						the_widget('WP_Widget_Categories', '', 'before_widget=<section class="widget %s"><div class="widget-wrap">&after_widget=</div></section>&before_title=<h4 class="widget-title widgettitle">&after_title=</h4>');
+					endif; ?>
 				</div>
+
 				<div class="footer-widgets-2 widget-area">
-					<?php the_widget('WP_Widget_Archives', '', 'before_widget=<section class="widget %s"><div class="widget-wrap">&after_widget=</div></section>&before_title=<h4 class="widget-title widgettitle">&after_title=</h4>'); ?>
+					<?php if (is_active_sidebar('footer-sidebar-2')) :
+						dynamic_sidebar('footer-sidebar-2');
+					else :
+						the_widget('WP_Widget_Archives', '', 'before_widget=<section class="widget %s"><div class="widget-wrap">&after_widget=</div></section>&before_title=<h4 class="widget-title widgettitle">&after_title=</h4>');
+					endif; ?>
 				</div>
+
 				<div class="footer-widgets-3 widget-area">
-					<?php the_widget('WP_Widget_Pages', '', 'before_widget=<section class="widget %s"><div class="widget-wrap">&after_widget=</div></section>&before_title=<h4 class="widget-title widgettitle">&after_title=</h4>'); ?>
+					<?php if (is_active_sidebar('footer-sidebar-3')) :
+						dynamic_sidebar('footer-sidebar-3');
+					else :
+						the_widget('WP_Widget_Pages', '', 'before_widget=<section class="widget %s"><div class="widget-wrap">&after_widget=</div></section>&before_title=<h4 class="widget-title widgettitle">&after_title=</h4>');
+					endif; ?>
 				</div>
 			</div>
 		</div>
