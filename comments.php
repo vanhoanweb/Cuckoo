ff0f961d215be90b71d20624384a16a6b0bf8627
Van Hoan <vanhoanweb@gmail.com>
2017-06-09 14:19:58 +0700

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,46 +1,45 @@
-<?php
+<div id="comments" class="entry-comments">
 
-if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME'])) {
-	die (__('Please do not load this page directly. Thanks!', 'zero-blank'));
-}
+	<?php if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME'])) {
+		die (__('Please do not load this page directly. Thanks!', 'zero-blank'));
+	}
 
-if (post_password_required()) {
-	echo '<p class="alert">'. __('This post is password protected. Enter the password to view comments.', 'zero-blank') .'</p>';
-	return;
-}
+	if (post_password_required()) {
+		echo '<p class="alert">'. __('This post is password protected. Enter the password to view comments.', 'zero-blank') .'</p>';
+		return;
+	} ?>
 
-?>
+	<?php if (have_comments()) : ?>
 
-<?php if (have_comments()) : ?>
-	
-	<h2 id="comments"><?php comments_number('No Responses', 'One Response', '% Responses');?></h2>
-	
-	<div class="navigation">		
-		<div class="next-posts"><?php previous_comments_link(); ?></div>
-		<div class="prev-posts"><?php next_comments_link(); ?></div>		
-	</div>
-	
-	<ol class="commentlist">		
-		<?php wp_list_comments(); ?>		
-	</ol>
-
-	<div class="navigation">		
-		<div class="next-posts"><?php previous_comments_link(); ?></div>
-		<div class="prev-posts"><?php next_comments_link(); ?></div>		
-	</div>
-	
- <?php else : // this is displayed if there are no comments so far ?>
-
-	<?php if (comments_open()) : ?>
+		<div class="comments-intro">
+			<span class="num-comments">
+				<?php comments_popup_link('Leave a comment', '1 Comment', '% Comments'); ?>
+			</span>
+			<a href="#commentform" class="leave-comment" rel="nofollow"><?php _e('Leave a Reply', 'zero-blank'); ?></a>
+		</div>
 		
-		<!-- If comments are open, but there are no comments. -->
+		<div class="navigation">
+			<div class="next-posts"><?php previous_comments_link(); ?></div>
+			<div class="prev-posts"><?php next_comments_link(); ?></div>		
+		</div>
 		
-	 <?php else : // comments are closed ?>
-	 	
-		<p><?php _e('Comments are closed.', 'zero-blank'); ?></p>
-		
+		<ol class="comment-list">
+			<?php wp_list_comments('type=comment&callback=zb_comments'); // Custom callback in functions.php ?>
+		</ol>
+
+		<div class="navigation">
+			<div class="next-posts"><?php previous_comments_link(); ?></div>
+			<div class="prev-posts"><?php next_comments_link(); ?></div>		
+		</div>
+	
+	<?php else : // this is displayed if there are no comments so far ?>
+		<?php if (comments_open()) : ?>			
+			<!-- If comments are open, but there are no comments. -->			
+		 <?php else : // comments are closed ?>		 	
+			<p><?php _e('Comments are closed.', 'zero-blank'); ?></p>			
+		<?php endif; ?>		
 	<?php endif; ?>
-	
-<?php endif; 
 
-if (comments_open()) comment_form(); ?>
+</div>
+
+<?php if (comments_open()) comment_form();
