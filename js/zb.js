d0d5cc3e69e8903f461a019821461039c11a0364
Van Hoan <vanhoanweb@gmail.com>
2017-06-13 17:31:19 +0700

    Create zb.js

--- js/zb.js
+++ js/zb.js
@@ -1,14 +1,14 @@
 (function($) {	
 	/* Responsive Menu */
-	$(".nav-primary .zb-nav-menu, .nav-secondary .zb-nav-menu").addClass("responsive-menu").before('<div class="responsive-menu-icon"></div>');
+	$(".nav-primary .zb-nav-menu").addClass("responsive-menu").before('<div class="responsive-menu-icon"></div>');
 	
 	$(".responsive-menu-icon").click(function() {
-		$(this).next(".nav-primary .zb-nav-menu, .nav-secondary .zb-nav-menu").slideToggle();
+		$(this).next(".nav-primary .zb-nav-menu").slideToggle();
 	});
 
 	$(window).resize(function() {
 		if(window.innerWidth > 767) {
-			$(".nav-primary .zb-nav-menu, .nav-secondary .zb-nav-menu, nav .sub-menu").removeAttr("style");
+			$(".nav-primary .zb-nav-menu, nav .sub-menu").removeAttr("style");
 			$(".responsive-menu > .menu-item").removeClass("menu-open");
 		}
 	});
