278155a8a69afc49f9ace3677d2a24bc0c38faa3
Van Hoan <vanhoanweb@gmail.com>
2017-06-16 17:28:24 +0700

    Update pagination.php

--- inc/pagination.php
+++ inc/pagination.php
@@ -1,18 +1,6 @@
-<?php if (is_singular('post')) : ?>
+<?php if (is_singular('post')) :
 
-	<section class="author-box" itemprop="author" itemscope="" itemtype="http://schema.org/Person">
-		<div class="author-box-avatar">
-			<?php echo get_avatar(get_the_author_meta('user_email'), 90, '', '', ['class' => 'alignleft']); ?>
-		</div>		
-		<h3 class="author-box-title">
-			<?php _e('About ', 'zero-blank'); ?>
-			<a class="author-box-link" href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>" rel="author">
-				<span itemprop="name"><?php echo get_the_author(); ?></span></a>
-		</h3>
-		<div class="author-box-content" itemprop="description">
-			<p><?php the_author_meta('description'); ?></p>
-		</div>
-	</section>
+	zb_author_box(); ?>
 
 	<div class="adjacent-entry-pagination pagination">
 		<?php previous_post_link('<div class="pagination-previous alignleft">%link</div>', '&larr; %title'); ?>
