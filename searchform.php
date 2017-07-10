fa0b94e8193d438b0a0196cd7fcea3b7b4b953d5
Van Hoan <vanhoanweb@gmail.com>
2017-07-10 10:47:11 +0700

    Delete searchform.php

--- searchform.php
+++
@@ -1,4 +0,0 @@
-<form class="search-form" itemprop="potentialAction" itemscope="" itemtype="http://schema.org/SearchAction" method="get" action="<?php echo home_url('/'); ?>" role="search">
-	<input type="search" class="search-field" name="s" id="s" placeholder="Search...">
-	<input type="submit" class="search-submit" value="Search">
-</form>
