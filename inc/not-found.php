1a64a753aa46a885777ecb0d7c147c79fcb10d2c
Van Hoan <vanhoanweb@gmail.com>
2017-07-10 10:42:28 +0700

    Delete not-found.php

--- inc/not-found.php
+++
@@ -1,3 +0,0 @@
-<div class="entry">	
-	<p><?php _e('Sorry, but the requested resource was not found on this site. Please try again or contact the administrator for assistance.', 'zero-blank'); ?></p>
-</div>
